cec7a3f83323899e6e66a0b21c80af3887efe375
Bradly Rowe <72034221+BradlyRowe@users.noreply.github.com>
2025-10-04 21:17:20 -0600

    updates

--- scripts/form.js
+++ scripts/form.js
@@ -1,18 +1,24 @@
 // Product array provided by the assignment
 const products = [
-  { id: "fc-1888", name: "Flux Capacitor" },
-  { id: "jj-2020", name: "Jetpack" },
-  { id: "ar-1995", name: "Arc Reactor" },
-  { id: "dp-2000", name: "Doomsday Device" },
-  { id: "tr-7070", name: "Time Turner" }
+    { id: "fc-1888", name: "Flux Capacitor" },
+    { id: "jj-2020", name: "Jetpack" },
+    { id: "ar-1995", name: "Arc Reactor" },
+    { id: "dp-2000", name: "Doomsday Device" },
+    { id: "tr-7070", name: "Time Turner" }
 ];
 
 // Populate the select dropdown
 const productSelect = document.querySelector("#product");
 
 products.forEach(product => {
-  const option = document.createElement("option");
-  option.value = product.id;
-  option.textContent = product.name;
-  productSelect.appendChild(option);
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
 });
+
+// Display Current Year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Display Last Modified Date
+document.getElementById("modified").textContent = document.lastModified;
